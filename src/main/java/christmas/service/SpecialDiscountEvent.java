5080c8ac993bff38f4b7d57c8284cabda8d8b5c5
besky <onlybesky@gmail.com>
2023-11-11 19:16:45 +0900

    feat: 특별 할인 기능 구현

---
+++ src/main/java/christmas/service/SpecialDiscountEvent.java
@@ -0,0 +1,30 @@
+package christmas.service;
+
+import static christmas.constant.Discount.SPECIAL;
+
+import christmas.constant.Discount;
+import christmas.util.DayUtil;
+import java.time.LocalDate;
+
+public class SpecialDiscountEvent implements DiscountEvent {
+    private final LocalDate visitDate;
+
+    public SpecialDiscountEvent(LocalDate visitDate) {
+        this.visitDate = visitDate;
+    }
+
+    @Override
+    public boolean appliable() {
+        return (DayUtil.isSunday(visitDate) || DayUtil.isChristmas(visitDate));
+    }
+
+    @Override
+    public Discount type() {
+        return SPECIAL;
+    }
+
+    @Override
+    public int price() {
+        return 0;
+    }
+}
