dbd40944987395c468f82cdc1b52a348bf8d667b
besky <onlybesky@gmail.com>
2023-11-11 19:18:14 +0900

    feat: 출력 형식 구현

---
+++ src/main/java/christmas/constant/Format.java
@@ -0,0 +1,12 @@
+package christmas.constant;
+
+public class Format {
+    public static final String PRICE_FORMAT = "###,###"; // DecimalFormat f = new DecimalFormat(PRICE_FORMAT); 이용
+    public static final String ORDER_FORMAT = "%s %d";
+    public static final String FREE_GIFT_FORMAT = "%s %d";
+    public static final String BENEFIT_DETAIL_FORMAT = "%s: ";
+    public static final String DASH = "-";
+    public static final String COMMA = ",";
+    public static final String BLANK = "";
+    public static final String SPACE = " ";
+}
