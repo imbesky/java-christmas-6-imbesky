c08abddea4f716a68d9dec68833dc2ae035b75ee
besky <onlybesky@gmail.com>
2023-11-12 15:37:29 +0900

    fix: 잘못된 값을 입력받을 경우 재입력받아 정상적인 값을 저장할 수 있도록 수정

--- src/main/java/christmas/controller/InputController.java
+++ src/main/java/christmas/controller/InputController.java
@@ -11,25 +11,39 @@ public class InputController {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
-    public VisitDate inputVisitDate() {
+    public VisitDate saveVisitDate() {
+        VisitDate visitDate;
+        do {
+            visitDate = inputVisitDate();
+        } while (visitDate == null);
+        return visitDate;
+    }
+
+    private VisitDate inputVisitDate() {
         outputView.askVisitDate();
         try {
             return new VisitDate(inputView.input());
         } catch (IllegalArgumentException e) {
             outputView.printError(e.getMessage());
-            inputVisitDate();
+            return null;
         }
-        return null;
     }
 
-    public Order inputOrder() {
+    public Order saveOrder() {
+        Order order;
+        do {
+            order = inputOrder();
+        } while (order == null);
+        return order;
+    }
+
+    private Order inputOrder() {
         outputView.askOrder();
         try {
             return new Order(toOrderFormat(inputView.input()));
         } catch (IllegalArgumentException e) {
             outputView.printError(e.getMessage());
-            inputOrder();
+            return null;
         }
-        return null;
     }
 }
