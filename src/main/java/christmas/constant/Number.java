9d90924de104e485401b6345a599f6f334c484ed
besky <onlybesky@gmail.com>
2023-11-13 16:16:47 +0900

    refactor: 주석 삭제

--- src/main/java/christmas/constant/Number.java
+++ src/main/java/christmas/constant/Number.java
@@ -8,6 +8,6 @@ public class Number {
     public static final int FIRST_INDEX = 0;
     public static final int SECOND_INDEX = 1;
     public static final int PROPER_ORDER_LENGTH = 2;
-    public static final int FREE_GIFT_NUMBER = 1; //TODO 이름 바꾸기
+    public static final int FREE_GIFT_NUMBER = 1;
     public static final int NEGATIVE = -1;
 }
