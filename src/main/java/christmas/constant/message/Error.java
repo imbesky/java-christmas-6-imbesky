d2ccf6c81880d7e18ea0a6481eaa57be36b0c878
besky <onlybesky@gmail.com>
2023-11-14 18:35:02 +0900

    chore: 중복된 `;` 삭제

--- src/main/java/christmas/constant/message/Error.java
+++ src/main/java/christmas/constant/message/Error.java
@@ -2,7 +2,7 @@ package christmas.constant.message;
 
 public enum Error {
     INVALIDATE_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    INVALIDATE_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요.");;
+    INVALIDATE_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private final String error;
     private static final String ERROR = "[ERROR] ";
