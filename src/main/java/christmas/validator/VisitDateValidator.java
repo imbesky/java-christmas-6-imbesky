941213b218815abff358796d931c2c17c8e9679c
besky <onlybesky@gmail.com>
2023-11-15 15:12:29 +0900

    refactor: 자바 API를 사용하도록 변경
    - 생성한 상수 대신 자바 API를 이용해 입력된 날짜가 1일에서 31일 사이인지 검증하도록 수정

--- src/main/java/christmas/validator/VisitDateValidator.java
+++ src/main/java/christmas/validator/VisitDateValidator.java
@@ -1,10 +1,13 @@
 package christmas.validator;
 
-import static christmas.constant.StandardDate.EVENT_END_DAY_OF_MONTH;
-import static christmas.constant.StandardDate.EVENT_START_DAY_OF_MONTH;
+import static christmas.constant.StandardDate.EVENT_MONTH;
+import static christmas.constant.StandardDate.EVENT_YEAR;
 import static christmas.constant.message.Error.INVALIDATE_DATE;
 import static christmas.constant.message.Tip.DATE_NOT_IN_RANGE;
 import static christmas.constant.message.Tip.DATE_NOT_NUMERIC;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
 
 public class VisitDateValidator {
     public void isNumeric(final String input) {
@@ -18,7 +21,9 @@ public class VisitDateValidator {
 
     public void isProperDateRange(final String input) {
         int date = Integer.parseInt(input);
-        if (date < EVENT_START_DAY_OF_MONTH || date > EVENT_END_DAY_OF_MONTH) {
+        try {
+            LocalDate.of(EVENT_YEAR, EVENT_MONTH, date);
+        } catch (DateTimeException e) {
             throw new IllegalArgumentException(INVALIDATE_DATE.getMessage()
                     .concat(DATE_NOT_IN_RANGE.getMessage()));
         }
